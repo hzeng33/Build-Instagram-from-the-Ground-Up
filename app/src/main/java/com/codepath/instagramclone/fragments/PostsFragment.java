8cbbec4276cbaa6958f447180182fd3ec19fe848
hzeng33 <hzeng9310@gmail.com>
2021-03-14 17:29:37 -0400

    Commit second part of the project

---
+++ app/src/main/java/com/codepath/instagramclone/fragments/PostsFragment.java
@@ -0,0 +1,133 @@
+package com.codepath.instagramclone.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.codepath.instagramclone.Post;
+import com.codepath.instagramclone.PostsAdapter;
+import com.codepath.instagramclone.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PostsFragment extends Fragment {
+
+    public static final String TAG = "PostsFragment";
+    private RecyclerView rvPosts;
+    protected PostsAdapter adapter;
+    protected List<Post> allPosts;
+    private SwipeRefreshLayout swipeContainer;
+
+    public PostsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(),allPosts);
+
+        //Steps to use the recycler view:
+        //1.Create layout for one row in the list.
+        //2.Create the adapter.
+        //3.Create the data source.
+        //4.Set the adapter on the recycler view.
+        rvPosts.setAdapter(adapter);
+        //5.Set the layout manager on the recycler view.
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                loadMoreData();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
+        queryPost();
+
+    }
+
+    public void loadMoreData() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.addDescendingOrder(Post.KEY_CREATEDAT_AT);   //make sure to show the most recent post.
+        query.setLimit(20);
+        query.whereGreaterThan(Post.KEY_CREATEDAT_AT,allPosts.get(0).getCreatedAt());
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG,"Issue with getting posts.",e);
+                    return;
+                }
+                for(Post post:posts){
+                    Log.i(TAG,"Username: "+ post.getUser().getUsername()+ ", Post: "+post.getDescription());
+                }
+
+                adapter.addAll(posts);
+                swipeContainer.setRefreshing(false);
+            }
+        });
+    }
+
+    protected void queryPost() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATEDAT_AT);   //make sure to show the most recent post.
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG,"Issue with getting posts.",e);
+                    return;
+                }
+                for(Post post:posts){
+                    Log.i(TAG,"Username: "+ post.getUser().getUsername()+ ", Post: "+post.getDescription());
+                }
+                allPosts.addAll(posts);
+                //Notify the data adapter.
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+}
